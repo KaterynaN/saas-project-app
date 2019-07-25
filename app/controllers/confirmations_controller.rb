05debafc36623fb939e6d6f6548bfc6b6b5e2e62
KaterynaN <kateryna.od.ua@gmail.com>
2019-07-26 00:04:00 +0300

    Fix confirmation controller

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -35,7 +35,7 @@ class ConfirmationsController < Milia::ConfirmationsController
       self.resource = resource_class.confirm_by_token(params[:confirmation_token])
       yield resource if block_given?
       if resource.errors.empty?
-        set_flash_message(:notice, :confirmed) if is_flashing format?
+        set_flash_message(:notice, :confirmed) if is_flashing_format?
       end
       if @confirmable.skip_confirm_change_password
         sign_in_tenanted_and_redirect(resource)
