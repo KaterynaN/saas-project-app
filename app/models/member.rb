e86f664f9d3f46d659b821b505b4ce343008e8d9
KaterynaN <kateryna.od.ua@gmail.com>
2019-07-29 23:51:29 +0300

    Complete artifacts

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,16 +1,16 @@
 class Member < ApplicationRecord
-   
+
   belongs_to :user
   acts_as_tenant
 
   DEFAULT_ADMIN = {
-    first_name: "Admin",
-    last_name:  "Please edit me"
+      first_name: "Admin",
+      last_name: "Please edit me"
   }
 
   def self.create_new_member(user, params)
     # add any other initialization for a new member
-    return user.create_member( params )
+    return user.create_member(params)
   end
 
   def self.create_org_admin(user)
@@ -20,7 +20,7 @@ class Member < ApplicationRecord
     end
 
     return new_member
-      
+
   end
 
 end
